7d450528ebb2c36dd5166f530797914bd4e32947
adeluca <adeluca@thinkingcapital.ca>
2019-10-20 17:44:44 -0400

    added feature to return only assigned ingredients and tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,7 +17,16 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new tag"""
@@ -43,11 +52,9 @@ class RecipeViewset(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-
     def _params_to_ints(self, qs):
         """Convert a list of string Ids to a list of integers"""
         return [int(str_id) for str_id in qs.split(',')]
-
 
     def get_queryset(self):
         """Limit objects to authenticated user"""
